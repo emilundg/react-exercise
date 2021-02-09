d36ec00af56a6f245012328fbce168c77ebca7b3
emilundg <lundgrenemil@hotmail.com>
2021-02-09 10:40:08 +0100

    REMOVE files

--- src/components/Task.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import './styles/Task.css'
-const Task = ({value, emitClick}) => {
-    return <div className="Task" onClick={() => emitClick()}>{value}</div>
-}
-export default Task
