ad92fd50fefef62a46432b6ef515bc79ec4b1ddf
emilundg <lundgrenemil@hotmail.com>
2021-02-09 14:01:25 +0100

    ADD example structure

--- main.js
+++ main.js
@@ -2,4 +2,23 @@ main()
 
 function main() {
     console.log("Hello World")
+
+    /*
+        Här kommer all kod att skrivas. För t.ex. första övningen för att hitta minsta numret i en array 
+        behöver man först definiera datan som följande: 
+    */
+
+    // Definiera först testdatan 
+    const testData = [30, 45, 60, 8]
+
+    // Kalla på funktionen: 
+    const result = findSmallestNumber(testData)
+    
+    // Logga resultatet!
+    console.log(result)
+}
+
+
+function findSmallestNumber(numberArray) {
+    // TODO: Implement
 }
