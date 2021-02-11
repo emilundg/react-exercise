3b15697f22c8300770f22181f1e841bf4309c905
emilundg <lundgrenemil@hotmail.com>
2021-02-11 10:37:20 +0100

    ADD comment

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,10 +2,22 @@ import React from 'react'
 import TodoList from './TodoList'
 
 class MainContent extends React.Component {
+    state = {
+        todoList: []
+    }
+    addTodo(item) {
+        /* TODO implement
+            1. assign todoList from state to a variable. aka const goodVariableName = this.state.todoList
+
+            2. add item to the assigned array
+
+            3. update state with this.setState({todoList: goodVariableName}) 
+        */
+    }
     render() {
         return (
             <div className="MainContent">
-                <TodoList/>
+                <TodoList todos={this.state.todos}/>
             </div>
         )
     }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const TodoList = () => {
+const TodoList = ({todos}) => {
     return (
-        <div className="TodoList">TodoList</div>
+        <div className="TodoList">
+            {/* Currently this div does not return anything but we can see that a prop, aka data from the parent is 
+            sent to TodoList.
+            
+            1. Assure that the type is array by checking the parent and see what data is sent to
+
+            2. In order to render out array items we have to iterate and return e.g a div. This can be done by using the method map inside our return
+
+            3. Make use of {todos.map(item => {
+                return <div> { item } </div>
+            }) }
+            
+            */}
+        </div>
     )
 }
 export default TodoList
